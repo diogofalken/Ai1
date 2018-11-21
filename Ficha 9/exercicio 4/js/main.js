41e56a17b8e70e75ffd44ef1a48dc3ff6187f37c
diogofalken <diogofalken0@gmail.com>
2018-11-21 14:45:06 +0000

    Ended F9E4

--- Ficha 9/exercicio 4/js/main.js
+++ Ficha 9/exercicio 4/js/main.js
@@ -2,7 +2,7 @@ var button = document.querySelector("button");
 var input = document.querySelector("input");
 
 button.addEventListener("click", function() {
-  var random_number = 3;
+  var random_number = Math.random() * 10;
   if (input.value == random_number) {
     alert("Acertou no numero!");
     input.disabled = "disabled";
